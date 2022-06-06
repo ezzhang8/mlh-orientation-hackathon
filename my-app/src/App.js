9193efebfaba33e70c7b57ad57d9603dc7bbf0bd
Eric Zhang <erizha.grad2022@my.stmc.bc.ca>
2022-06-06 01:55:42 -0700

    Removed boilerplate code and added testing threshold

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,78 +7,42 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import axios from "axios";
 import { Buffer } from "buffer"
-
+import Overlay from './components/Overlay';
 
 const endpoint = "https://hf.space/embed/RaymondDashWu/mlh_hackathon_sum22/+/api/predict/";
 
 function App() {
-  // const container = document.createElement('div');
-  // container.style.width = '400px';
-  // container.style.height = '300px';
-  // document.body.append(container);
-
-
-  // const viewer = new Viewer({
-  //   accessToken: "MLY|5494923973921616|75ede84ae518fed4232a6e7eb7d53688",
-  //   container: container,
-  //   imageId: "1360724954411676",
-  // });
-
-  const [imageId, setImageId] = useState("1360724954411676");
+  const [predict, setPredict] = useState("");
+  const imageId = "1360724954411676";
 
   async function predictImage(url) {
     let image = await axios.get(url, { responseType: 'arraybuffer' });
     let raw = Buffer.from(image.data).toString('base64');
     let string = "data:" + image.headers["content-type"] + ";base64," + raw;
 
-    let predict = await axios.post(endpoint, {data: [string]})
+    let predict = await axios.post(endpoint, {data: [string]});
 
-    console.log(predict.data);
+    console.log(predict.data.data[0].confidences[1].confidence);
 
+    if (await predict.data.data[0].confidences[1].confidence > 0.4) {
+      setPredict("There may be a pothole here!");
+    }
+    else {
+      setPredict("");
+    }
   }
 
   return (
     <>
-
-      {/* {console.log(viewer.image)} */}
-      {/* <Navbar bg="light" expand="lg">
-        <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar> */}
-
-
       <div>
-        {/* <MapillaryViewer
-          clientId="5494923973921616"
-          imageKey="1360724954411676"
-          filter={['==', 'userKey', '2PiRXqdqbY47WzG6CRzEIA']}
-          onTiltChanged={tilt => console.log(`Tilt: ${tilt}`)}
-          onFovChanged={fov => console.log(`FoV: ${fov}`)}
-          onNodeChanged={node => console.log(node)}
-          onBearingChanged={bearing => console.log(`Bearing: ${bearing}`)}
-        /> */}
-
         <div>
           <MapillaryViewer
             imageId={imageId}
             onImageChanged={image => predictImage(image.image._spatial.thumb.url)}
           />
         </div>
+        {predict !== "" && <Overlay text={predict}></Overlay>}
+       
       </div>
     </>
 
